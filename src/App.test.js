4fd2c26e142b8668e1c6c0db975d6dfdb4dbff67
felix.pham <felix.pham@vmogroup.com>
2025-01-01 21:56:25 +0700

    second commit

---
+++ src/App.test.js
@@ -0,0 +1,10 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+import '@testing-library/jest-dom'
+
+describe('App', () => {
+  it('should render while authenticating', () => {
+      render(<App />);
+      expect(screen.getByText('Vite + Reactooooo')).toBeInTheDocument();
+  });
+});
